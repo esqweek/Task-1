f95a10b96a698966f89b99ce346b7dfcd58dacce
esqweek <115408408+esqweek@users.noreply.github.com>
2022-10-09 16:40:48 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿// Fahmie DH
+// Task 1
+// 09.10.2022
+
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	int fd, sd;
+	sd = a % 10;
+	sd *= 10;
+	cout << sd;	
+	fd = sd - 30;
+	cout << fd;
+
+}
